2d0ded405346ac64696a4a423b159c8c29a669fb
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-02-03 18:19:41 +0530

    Production Ready

--- Day-9/Backend/src/app.js
+++ Day-9/Backend/src/app.js
@@ -2,14 +2,19 @@
 const express = require("express"); // requiring express package
 const app = express(); //creating server instance :
 const notesRoutes = require("./routes/notes.routes.js")
-const cors = require("cors")
+const cors = require("cors");
+const path = require("path");
 
 app.use(express.json()); // Making capable the server for read data in req.body;
 app.use(cors()); //start accepting the cross origin requests on server :
+app.use(express.static("./public"))
 
 // Handle request in routes
 
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
+app.use("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+})
 
 
 module.exports = app;
