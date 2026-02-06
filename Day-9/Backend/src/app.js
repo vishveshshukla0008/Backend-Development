3ea8237f66af6958be0e3082e05ffa82ece4835f
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-02-06 23:06:32 +0530

    Day-9

--- Day-9/Backend/src/app.js
+++ Day-9/Backend/src/app.js
@@ -3,7 +3,7 @@ const express = require("express"); // requiring express package
 const app = express(); //creating server instance :
 const notesRoutes = require("./routes/notes.routes.js")
 const cors = require("cors");
-const path = require("path");
+const path = require("path"); // Node js module
 
 app.use(express.json()); // Making capable the server for read data in req.body;
 app.use(cors()); //start accepting the cross origin requests on server :
